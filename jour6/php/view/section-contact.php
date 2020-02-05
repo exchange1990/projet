cdb48e41449ce516a963fc0cf095a62dd04bcac7
exchange1990 <exchangezak@gmail.com>
2020-02-05 16:28:17 +0100

    php

--- jour6/php/view/section-contact.php
+++ jour6/php/view/section-contact.php
@@ -1,4 +1,4 @@
 <main>
        <h2>contact</h2>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aperiam necessitatibus, in laborum atque, fugit neque officia odio eaque excepturi odit veritatis accusamus officiis veniam alias repellat. Impedit, suscipit veniam! </p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aperiam necessitatibus, in laborum atque, fugit neque officia odio eaque ex hhcepturi odit veritatis accusamus officiis veniam alias repellat. Impedit, suscipit veniam! </p>
 </main>
