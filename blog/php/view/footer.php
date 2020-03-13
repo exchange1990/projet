2aa97f061b5a8f353bac65937f89264813861c12
exchange1990 <exchangezak@gmail.com>
2020-03-13 14:53:19 +0100

    blog

--- blog/php/view/footer.php
+++ blog/php/view/footer.php
@@ -2,8 +2,6 @@
     <footer>
         <a href="admin.php">ADMIN</a>
         <p>tous droits réservés</p>
-        <!-- POUR ALLER SUR LA PAGE ADMIN -->
-        
     </footer>
     <script src="assets/js/app.js"></script>
 </body>
--- blog/php/view/section-news.php
+++ blog/php/view/section-news.php
@@ -4,38 +4,7 @@
             <h2>liste des articles EN PHP ET SQL (READ)</h2>
             <div class="listeArticle">
 <?php
-// ON VA FAIRE UNE BOUCLE POUR CREER CHAQUE ARTICLE
-// ET GENERALEMENT, ON A UN TABLEAU PHP SUR LEQUEL ON MET LA BOUCLE
-// => AVEC UN TABLEAU ASSOCIATIF, ON VA POUVOIR RECUPERER LES MEMES INFOS
-// EN PHP, UNE LIGNE SQL SERA UN TABLEAU ASSOCIATIF
-/*
-// LIGNE 1
-$tabAsso1 = [ 
-    "id"                => 1, 
-    "titre"             => "cornavirus", 
-    "contenu"           => "sera-t-il bloqué par les frontières ?", 
-    "image"             => "assets/img/photo1.jpg", 
-    "datePublication"   => "2020-03-03 00:00:00", 
-    "categorie"         => "sante"
-];
-// LIGNE 2
-$tabAsso2 = [ 
-    "id"                => 2, 
-    "titre"             => "benjamin griveaux", 
-    "contenu"           => "il est passé où le benjamin ?", 
-    "image"             => "assets/img/photo1.jpg", 
-    "datePublication"   => "2020-03-03 00:00:00", 
-    "categorie"         => "politique"
-];
-// ON VA UTILISER UN TABLEAU INDEXE DE TABLEAU ASSOCIATIFS
-$tabLigne = [
-    $tabAsso1,
-    $tabAsso2,
-];
-*/
 
-// POUR RECUPERER LES INFOS DE SQL
-// ETAPE2: LANCER UNE REQUETE SQL EN LECTURE
 $requeteSQL =
 <<<CODESQL
 SELECT * FROM `articles`
@@ -45,26 +14,14 @@ CODESQL;
 
 $tabAssoColonneValeur = [];
 
-// JE CHARGE LE CODE PHP POUR ENVOYER LA REQUETE
+
 require_once "php/model/envoyer-sql.php";
 
-// ETAPE3: JE RECUPERE MON TABLEAU DE RESULTATS
-// https://www.php.net/manual/fr/pdostatement.fetchall.php
 $tabLigne = $pdoStatement->fetchAll();
 
-// ON FAIT UNE BOUCLE POUR AFFICHER CHAQUE ARTICLE
+
 foreach($tabLigne as $tabAsso)
 {
-    /*
-    $id         = $tabAsso["id"];
-    $titre      = $tabAsso["titre"];
-    $contenu    = $tabAsso["contenu"];
-    $image      = $tabAsso["image"];
-    $categorie  = $tabAsso["categorie"];
-    */
-    // SIMPLIFICATION
-    // https://www.php.net/manual/fr/function.extract.php
-    // extract CREE DES VARIABLES A PARTIR DES CLES DU TABLEAU ASSOCIATIF
     extract($tabAsso); 
 
     echo
@@ -76,10 +33,6 @@ foreach($tabLigne as $tabAsso)
     </article>
 CODEHTML;
 }
-
-// LES INFOS QUI NOUS INTERESSENT SONT DANS UNE TABLE SQL articles
-// LA TABLE SQL A DES COLONNES titre, image, contenu, etc... 
-// ET LES INFOS SONT GROUPEES DANS UNE MEME LIGNE
 
 ?>
         </section>
